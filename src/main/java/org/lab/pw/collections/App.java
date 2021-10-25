65665270e74b8a48e9155184287ef599f84d1150
koxx4 <piotr815@protonmail.com>
2021-10-25 10:27:52 +0200

    initial commit

---
+++ src/main/java/org/lab/pw/collections/App.java
@@ -0,0 +1,19 @@
+package org.lab.pw.collections;
+
+public class App 
+{
+    //array list
+    //sorted set
+    //vector
+    //map
+    //hash map
+    //linked hash map
+    //tree map
+    //set
+    //hash set
+    //tree set
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
