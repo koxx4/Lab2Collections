650a30ec2247f496cce5b8d223ef48801cee145e
koxx4 <piotr815@protonmail.com>
2021-10-25 16:16:35 +0200

    added constatns

--- src/main/java/org/lab/pw/collections/App.java
+++ src/main/java/org/lab/pw/collections/App.java
@@ -1,16 +1,17 @@
 package org.lab.pw.collections;
 
 import org.lab.pw.collections.demonstration.*;
-import org.lab.pw.collections.utils.ConsoleUserDialog;
 import org.lab.pw.collections.utils.Demonstrator;
 
-import java.util.Collection;
 import java.util.List;
 
 public class App {
 
+    static int DATA_SET_SIZE = 10;
+    static boolean IS_DATA_SET_CONSISTENT = true;
+
     public static void main( String[] args ) {
-        Demonstrator demonstrator = new Demonstrator(4, true);
+        Demonstrator demonstrator = new Demonstrator(DATA_SET_SIZE, IS_DATA_SET_CONSISTENT);
         try{
             demonstrator.startDemonstration(List.of(
                     new ArrayListDemonstration(),
